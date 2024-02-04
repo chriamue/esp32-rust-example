05c57e2576b9c396269a3a1c11f53fab3df79ab1
Christian M <chriamue@gmail.com>
2024-02-04 13:42:13 +0100

    :sparkles: adds some dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+use anyhow::Ok;
+use esp_idf_sys::esp_app_desc;
+
+esp_app_desc!();
+
+fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    Ok(())
 }
